90873310d4c2018eb35de69fc8743a68f40df204
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-05-24 18:48:11 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import profilePic from '../public/logo.jpg';
-import styles from './page.module.css';
+import Image from 'next/image'
+import profilePic from '/public/logo.jpg'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
