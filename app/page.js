38381c989f8b5c0c4235652281b3f8ae506b111e
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-05-25 09:14:45 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,15 +9,6 @@ export default function Home() {
           Hello!&nbsp;
           <code className={styles.code}>Lovely to meet and connect with you!</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            
-          </a>
-        </div>
       </div>
 
 
