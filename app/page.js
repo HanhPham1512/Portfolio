2e7a633360ed531e573917e0088ce7cfc37a8a77
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-05-24 21:31:39 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://new-portfolio-hanhpham1512.vercel.app/blog"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
